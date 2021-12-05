65da85ecfbe1f033b4a1b2b1ece87dec52249a68
Mark77645 <Marksamirf@gmail.com>
2021-12-05 15:48:43 +0200

    .

--- src/Network.java
+++ src/Network.java
@@ -8,5 +8,11 @@ public class Network {
 
     public static void main(String[] args){
 
+
+
+
+        System.out.println("What is the number of WI-FI Connections?");
+
+        System.out.println("What is the number of devices Clients want to connect?");
     }
 }
